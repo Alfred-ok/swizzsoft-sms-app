14a6262d5e7b05e940363fbc871ca7ffa8aee45c
Alfred Okwiri <okwirialfred@gmail.com>
2024-10-10 18:32:09 +0300

    favicon

--- src/components/AppSidebar.js
+++ src/components/AppSidebar.js
@@ -13,9 +13,9 @@ import CIcon from '@coreui/icons-react'
 
 import { AppSidebarNav } from './AppSidebarNav'
 
-import { logo } from 'src/assets/brand/logo'
-import { sygnet } from 'src/assets/brand/sygnet'
-import { useContext } from 'react'
+//import { logo } from 'src/assets/brand/logo'
+//import { sygnet } from 'src/assets/brand/sygnet'
+//import { useContext } from 'react'
 //import AuthContext from '../Context/AuthProvider'
 import Cookies from 'js-cookie';
 
